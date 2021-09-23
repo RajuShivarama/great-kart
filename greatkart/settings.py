a5b9117575d44d9dd53186ef826e3268435de854
RajuShivarama <rajushivaram97@gmail.com>
2021-09-23 14:49:49 +0530

    aws

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool) # True
 
-ALLOWED_HOSTS = ['greatkart-course-env.eba-pkaqpq2t.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['greatkart-app-env.eba-cbrakebt.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
